a6fac1e2fe9f44f0c9368cac6fd604d40c451efb
yang_ <tongvue@uwm.edu>
2021-04-09 17:20:19 -0500

    Avatar Update Image.

--- screens/RootDrawerScreen.js
+++ screens/RootDrawerScreen.js
@@ -7,28 +7,30 @@ import RegisterScreen from './RegisterScreen'
 import SplashScreen from './SplashScreen';
 import PasswordResetScreen from './PasswordResetScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DrawerContentScrollView, DrawerItemList, DrawerItem} from '@react-navigation/drawer';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import UploadImage from './UploadImage';
 
 const Drawer = createDrawerNavigator();
 
-const signOut = () => { 
+const signOut = () => {
     auth()
-    .signOut()
-    .then(() => console.log('User signed out!'));
+        .signOut()
+        .then(() => console.log('User signed out!'));
 }
 
-const CustomDrawerContent= (props)=>{
+const CustomDrawerContent = (props) => {
     return (
         <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem label="Sign Out" onPress={() => signOut()} />
-      </DrawerContentScrollView>
+            <DrawerItemList {...props} />
+            <DrawerItem label="Sign Out" onPress={() => signOut()} />
+        </DrawerContentScrollView>
     );
 }
 
-const RootDrawerScreen = ({navigation}) => (
+const RootDrawerScreen = ({ navigation }) => (
     <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="LoginScreen" component={LoginScreen}/>
+        <Drawer.Screen name="LoginScreen" component={LoginScreen} />
+        <Drawer.Screen name="UploadImage" component={UploadImage} />
     </Drawer.Navigator>
 );
 
--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -5,16 +5,18 @@ import LoginScreen from './LoginScreen'
 import RegisterScreen from './RegisterScreen'
 import SplashScreen from './SplashScreen';
 import PasswordResetScreen from './PasswordResetScreen';
+import UploadImage from './UploadImage';
 
 
 const RootStack = createStackNavigator();
 
-const RootStackScreen = ({navigation}) => (
+const RootStackScreen = ({ navigation }) => (
     <RootStack.Navigator headerMode="none">
-        <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
-        <RootStack.Screen name="LoginScreen" component={LoginScreen}/>
-        <RootStack.Screen name="RegisterScreen" component={RegisterScreen}/>
-        <RootStack.Screen name="PasswordResetScreen" component={PasswordResetScreen}/>
+        <RootStack.Screen name="SplashScreen" component={SplashScreen} />
+        <RootStack.Screen name="LoginScreen" component={LoginScreen} />
+        <RootStack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <RootStack.Screen name="PasswordResetScreen" component={PasswordResetScreen} />
+        <RootStack.Screen name="UploadImage" component={UploadImage} />
     </RootStack.Navigator>
 );
 
